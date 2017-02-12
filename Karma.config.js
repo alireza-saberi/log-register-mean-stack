644e58411c246b1e3e3839bed6ff4f5a9f0550ec
Alireza Saberi <saberi.ar.eng@gmail.com>
2017-02-12 17:59:35 -0500

    chore: configuring project
    Project is configured with proper packages, files, and directories.
    Empty folders stay on local until getting their contents.
    
    [ticket : #1]

---
+++ Karma.config.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Ali
+* @Date:   2017-02-12 17:38:09
+* @Last Modified by:   Ali
+* @Last Modified time: 2017-02-12 17:38:09
+*/
+
+'use strict';
---
+++ gruntfile.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Ali
+* @Date:   2017-02-12 17:37:06
+* @Last Modified by:   Ali
+* @Last Modified time: 2017-02-12 17:37:06
+*/
+
+'use strict';
